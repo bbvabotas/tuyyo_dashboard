3bfa8b49c52873579525f72142a7ff8e8f122a89
BBVA: Analytics <joel.chacon.contractor@bbva.com>
2017-08-18 10:29:36 -0500

    Added Mobile Download page

---
+++ src/main_pages/Mobile-Download.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-sm-12">
+                <h3>Mobile Download</h3>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="coming_soon">
+                COMING SOON
+            </div>
+        </div>
+        
+        <div class="row">
+            <div>
+                Download the mobile version of the Tuyyo dashboard so that you can see the dashboard anytime and anywhere on your phone
+            </div>
+        </div>
+        
+        
+    </div>
+</template>
+
+<script>
+    
+    export default {
+        mounted() {
+            
+        },
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+            
+        },
+        components: {
+            
+        }
+    }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .coming_soon {
+        font-size: 5em;
+        margin: 50px 0;
+    }
+</style>
